3bed8ab3cc4c48312f174f69d67c83746030290c
Craco <gabriel_craco@hotmail.com>
2024-09-06 19:23:58 -0300

    fix(workshop): remove authorization from list workshops

--- src/workshop/workshop.controller.ts
+++ src/workshop/workshop.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { WorkshopService } from './workshop.service';
 import { ResponseDTO } from 'src/utils/api-response.util';
+import { JWT } from 'src/decorators/jwt.decorator';
 
 @Controller('workshop')
 export class WorkshopController {
   constructor(private readonly workshopService: WorkshopService) {}
 
   @Get()
+  @JWT(false)
   async list() {
     return new ResponseDTO(HttpStatus.OK, 'Workshops has been listed', 'Oficinas listadas com sucesso', await this.workshopService.list());
   }
